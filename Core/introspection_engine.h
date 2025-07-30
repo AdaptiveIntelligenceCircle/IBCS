7652f33328153017fb22f383c8e74cd3a8547d4c
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-07-30 16:10:56 +0700

    Update introspection_engine.h

--- Core/introspection_engine.h
+++ Core/introspection_engine.h
@@ -1,1 +1,19 @@
+#pragma once 
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std; 
+
+namespace ibcs
+{
+    class IntrospectionEngine
+    {
+        public: 
+        void analyzeThoughtProcess(const vector<string> &ideastream);
+        string summarizeIntropection(); 
+        
+        private: 
+        vector<string> innerThoughts;
+    };  
+}
