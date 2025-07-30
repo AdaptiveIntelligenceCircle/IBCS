a7a0dbc716640e636b4e54a5d46a5abc7a715319
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-07-30 16:10:10 +0700

    Update idea_parser.h

--- Core/idea_parser.h
+++ Core/idea_parser.h
@@ -1,1 +1,39 @@
+#pragma once
 #include <iostream>
+#include <string> 
+#include <memory> 
+
+#include "behavior_model.h"
+#include "context_manager.h"
+using namespace std; 
+
+namespace ibcs
+{
+    struct ParsedIdea
+    {
+        string raw_input;
+        string Concept; 
+        vector<string> modifiers; 
+        double confidence;
+
+        ParsedIdea(const string &input, const string &c, const vector<string> &m, double conf);
+
+    }; 
+
+    class IdeaParser
+    {
+        public: 
+        BehaviorModel parseRawIdea(const string &rawText);
+        ParsedIdea parse(const string &raw_idea); 
+        void updateContext(const ParsedIdea &idea); 
+
+        IdeaParser(shared_ptr<ContextManager> context);
+        
+        private: 
+        shared_ptr<ContextManager> context_manager;
+
+        string extractConcept(const string &input); 
+        vector<string> extractModifiers(const string &input);
+        double estimateConfidence(const string &input);  
+    };
+}
