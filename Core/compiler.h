e3c5885a54756832148a6cf33b0234cfc534f216
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-07-30 16:08:32 +0700

    Update compiler.h

--- Core/compiler.h
+++ Core/compiler.h
@@ -1,1 +1,23 @@
 #include <iostream>
+#pragma once
+#include <string> 
+
+#include "behavior_model.h"
+#include "context_manager.h"
+
+using namespace std;
+
+namespace ibcs
+{
+    class Compiler
+    {
+        public: 
+        Compiler(); 
+
+        // Bien dich hanh vi (tu model) thanh logic cu the...// 
+        string compileBehavior(const BehaviorModel &model, const ContextManager &context ); 
+
+        private: 
+        void optimizeCompiledCode(string &code); 
+    }; 
+}
