ad9a6804b8f94bca335f3e829f2da44dd3ff0d97
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-07-30 16:35:43 +0700

    Update interpreter.h

--- language/interpreter.h
+++ language/interpreter.h
@@ -1,2 +1,14 @@
 #include <iostream> 
 using namespace std; 
+
+#include <memory> 
+#include "grammar_def.h"
+class Interpreter
+{
+    public: 
+    void execute(const shared_ptr<ASTNODE> &node);
+    void eval(const shared_ptr<ASTNODE> &node); 
+    
+    private:
+    GrammarRule env;
+}; 
