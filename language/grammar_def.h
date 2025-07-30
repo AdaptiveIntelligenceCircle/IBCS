8ecf58279f88457ec1b59706553541cf138f1272
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-07-30 16:36:04 +0700

    Update grammar_def.h

--- language/grammar_def.h
+++ language/grammar_def.h
@@ -1,2 +1,27 @@
 #include <iostream>
 using namespace std; 
+#include <vector>
+#include <memory>
+enum class TokenType
+{
+    Identifier, 
+    Number,
+    String, 
+    Keyword, 
+    Symbol, 
+    EndOfFile,
+    // .... // 
+};
+
+struct GrammarRule
+{
+    string name; 
+    vector<string> production; 
+}; 
+
+struct ASTNODE
+{
+    string type;
+    vector<shared_ptr<ASTNODE>> children;
+    string value;
+}; 
