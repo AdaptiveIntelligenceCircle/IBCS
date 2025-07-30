4a101771ea9addfed67dbc164b5a555d15e69126
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-07-30 16:08:06 +0700

    Update compiler.cpp

--- Core/compiler.cpp
+++ Core/compiler.cpp
@@ -1,1 +1,20 @@
 #include "compiler.h"
+namespace ibcs
+{
+    Compiler :: Compiler(){}
+
+    string Compiler :: compileBehavior(const BehaviorModel &model , const ContextManager &context)
+    {
+        string code; 
+        for (const auto &node : model.getNodes())
+        {
+            code += "if(" + node.condition +")" + node.action;
+        }
+        optimizeCompiledCode(code);
+        return code; 
+    }
+
+    void Compiler :: optimizeCompiledCode(string &code){
+        // Toi uu ma nguon o muc thap (loai bo du thua logic)
+    }
+}
