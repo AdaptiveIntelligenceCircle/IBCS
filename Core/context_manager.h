e41aef79c43c558d3c68ceb2874a763283fef944
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-07-30 16:09:16 +0700

    Update context_manager.h

--- Core/context_manager.h
+++ Core/context_manager.h
@@ -1,1 +1,21 @@
+#pragma once 
 #include <iostream>
+#include <unordered_map> 
+#include <string>
+
+using namespace std; 
+
+namespace ibcs
+{
+    class ContextManager
+    {
+        public: 
+        void setContextValue(const string &key, const string &value);
+        string getContextValue(const string &key) const;  
+        void clearContext(); 
+        
+
+        private: 
+        unordered_map<string, string> context; 
+    } ; 
+}
