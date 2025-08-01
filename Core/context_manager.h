4839331f7bdae4e1f639f27cca4c01ecc4cbd500
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-08-01 22:23:42 +0700

    Update context_manager.h

--- Core/context_manager.h
+++ Core/context_manager.h
@@ -13,9 +13,17 @@ namespace ibcs
         void setContextValue(const string &key, const string &value);
         string getContextValue(const string &key) const;  
         void clearContext(); 
-        
+        void setValue(const string &scope, const string &key, const string &value); 
+        string getvalue(const string &scope, const string &key, const string &value);
+        void pushSnapshot();  
+        void rollback(); 
+        void logChanges(); 
+        void localFromJson(const string &filepath); 
+        void saveToJson(const string &filepath); 
 
         private: 
-        unordered_map<string, string> context; 
+        unordered_map<string, string> context;
+        unordered_map<string, unordered_map<string, string>> scoped; 
+        vector<string>changeLog;
     } ; 
 }
