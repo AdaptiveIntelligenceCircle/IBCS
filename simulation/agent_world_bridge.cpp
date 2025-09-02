78f145d044da3e65054f7c10e1f212d3c6ee3cc7
TriNguyenDuc <163371562+CodelikeC@users.noreply.github.com>
2025-09-02 16:37:28 +0700

    Update agent_world_bridge.cpp

--- simulation/agent_world_bridge.cpp
+++ simulation/agent_world_bridge.cpp
@@ -1,2 +1,31 @@
-#include <iostream> 
-using namespace std;
+#pragma once 
+#include <string> 
+#include <vector> 
+#include <iostream>
+
+namespace simulation
+{
+    struct Observation
+    {
+        std :: vector<float> sensor_data; 
+    }; 
+
+    struct Action
+    {
+        std :: vector<float> motor_commands; 
+    }; 
+
+    class AgentWorldBridge
+    {
+        public: 
+        AgentWorldBridge(); 
+        ~AgentWorldBridge(); 
+
+        void initialize(); 
+        Observation sense_from_world(); 
+        void send_action_to_world(const Action &action) ;
+
+        private: 
+        bool initialized_;
+    };  
+}   
