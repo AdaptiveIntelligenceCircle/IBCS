b6b8e5362853c26e84543774d95c896e50ce84fc
ndtribk <ndtribk@gmail.com>
2025-11-18 12:30:46 +0700

    loaded_plugin.manifest.json

--- plugins/plugins_manager.h
+++ plugins/plugins_manager.h
@@ -13,5 +13,31 @@
 using namespace std ; 
 
 namespace ibcs :: plugin{
-    
-}
+    class PluginManager
+    {
+        public: 
+        PluginManager(); 
+        ~PluginManager(); 
+
+        // load plugin manifest from file(json) and attempt to load plugin.. 
+        bool load_plugin_from_file(const string &manifest_path); 
+
+        // unload by plugin id (manifest.name)..// 
+        bool unload_plugin(const string &plugin_id);
+
+        // list loaded plugin ids 
+        vector<string> list_plugin() const; 
+
+        // QUery capabilities across plugins..//
+        vector<string> find_plugins_with_capabilities(const string &cap) const; 
+
+        private: 
+        bool load_native(const PluginManifest &m); 
+        bool load_lua(const PluginManifest &m); 
+        bool load_wasm(const PluginManifest &m); 
+
+        SandboxPolicy default_policy_for(const PluginManifest &m) const; 
+    }; 
+
+    unique_ptr<PluginManager> make_plugin_manager(); 
+} // namespace ibcs :: plugin.. 
